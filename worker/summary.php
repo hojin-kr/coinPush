d81bcca38ea5568dabada065c37ca021b05d761e
hojin <jhj377@gmail.com>
2021-10-16 12:24:24 +0900

    비율조종

--- worker/summary.php
+++ worker/summary.php
@@ -31,13 +31,13 @@ function persentageTop($exchange, $root, $name) {
     if(!empty($ticks)) {
         krsort($ticks);
         $message = "\n[$name]\n\n";
-        foreach(array_splice($ticks,0,5) as ['symbol'=>$symbol, 'percentage'=>$percentage]) {
+        foreach(array_splice($ticks,0,8) as ['symbol'=>$symbol, 'percentage'=>$percentage]) {
             $percentage = (int)$percentage;
             $message .= "$symbol : $percentage% \n";
         }
         ksort($ticks);
         $message .= "--------------------\n";
-        foreach(array_splice($ticks,0,5) as ['symbol'=>$symbol, 'percentage'=>$percentage]) {
+        foreach(array_splice($ticks,0,2) as ['symbol'=>$symbol, 'percentage'=>$percentage]) {
             $percentage = (int)$percentage;
             $message .= "$symbol : $percentage% \n";
         }
